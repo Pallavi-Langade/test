5f08e469fba068633912d600021e050e500a1994
Pallavi Langade <pallavilangade188@gmail.com>
2021-01-13 16:04:48 +0530

    New code added

--- new.py
+++ new.py
@@ -1,5 +1,19 @@
-name = list(map(str,input("Enter the name").split()))
-print(name)
+import math
+class Circle():
+    def __init__(self,r):
+        self.radius = r
+    
+    def getArea(self):
+        area = math.pi * math.pow(self.radius,  2)
+        print("Area of circle: ",area)
+    
+    def getCircumference(self):
+        circum_fere = 2* math.pi * self.radius
+        print("The Circumference of the circle is: ", circum_fere)
 
-color = str(input("Enter the color name: "))
-print(color)
+    def getArea22(self):
+        print("3.14*self.radius*self.radius")
+c1 = Circle(7)
+c1.getArea()
+c1.getCircumference()
+
